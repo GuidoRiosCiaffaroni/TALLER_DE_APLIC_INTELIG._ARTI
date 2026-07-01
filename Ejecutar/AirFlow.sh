c4160600ef6a6620418dc940dab457ee9e39f2da
Guido Rios Ciaffaroni <guidoriosciaffaroni@gmail.com>
2026-06-30 23:25:24 -0400

    2026_06_30_23_25_24

--- Ejecutar/AirFlow.sh
+++ Ejecutar/AirFlow.sh
@@ -1,14 +1,14 @@
+#!/bin/bash
+
 # 1. Navegar a la carpeta del proyecto
 cd /root/airflow_project
 
-# 2. Activar el entorno virtual de Python
-source airflow_env/bin/activate
+# 2. Activar el entorno usando el punto (.) para compatibilidad universal
+. airflow_env/bin/activate
 
-# 3. Informar a Airflow dónde leer las configuraciones
+# 3. Declarar la ruta de configuración
 export AIRFLOW_HOME=/root/airflow_project/airflow
 
-# 4. Levantar el Webserver en segundo plano (así liberas la consola)
+# 4. Levantar los servicios
 airflow webserver -p 8080 &
-
-# 5. Levantar el Scheduler (este se quedará mostrando logs en tiempo real)
 airflow scheduler
